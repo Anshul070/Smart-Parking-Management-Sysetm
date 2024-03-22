7d0c7d912d95d1319e0ea5b0f07829edbdb6071a
Anshul <sainianshul070@gmail.com>
2024-03-22 21:59:31 +0530

    Input From two IR sensiors can be readed

--- nodeMCU_code/nodeMCU_code.ino
+++ nodeMCU_code/nodeMCU_code.ino
@@ -1,13 +1,17 @@
 int IR = D3;
+int IR2 = D4;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(IR,INPUT);
+  pinMode(IR2,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int buttonState = digitalRead(IR);
+  int buttonState2 = digitalRead(IR2);
   Serial.println(buttonState);
+  Serial.println(buttonState2);
   delay(1);
 }
