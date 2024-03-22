2b1823d9cb285156a5426be2905537dab4c1493a
Anshul <sainianshul070@gmail.com>
2024-03-22 21:53:01 +0530

    Inout from IR sensors can be readed

---
+++ nodeMCU_code/nodeMCU_code.ino
@@ -0,0 +1,13 @@
+int IR = D3;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(IR,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int buttonState = digitalRead(IR);
+  Serial.println(buttonState);
+  delay(1);
+}
